6808f84493294b9ce9cf5a5a7b6c86b8949062ce
balajipalakkattu <balajipalakkattu@gmail.com>
2020-02-09 01:53:16 -0600

    Unit test: getall

---
+++ src/test/java/com/example/customer/controller/CustomerControllerTest.java
@@ -0,0 +1,48 @@
+package com.example.customer.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.customer.model.Customer;
+import com.example.customer.service.CustomerService;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class CustomerControllerTest {
+
+	@Mock
+	private CustomerService customerService;
+	
+	@InjectMocks
+	private CustomerController customerController;
+	
+	@BeforeClass
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		when(customerService.getCustomers()).thenReturn(new ArrayList<Customer>() {{add(new Customer());}});
+		
+	}
+	@org.junit.jupiter.api.Test
+	public void getAll() {
+		assertEquals("The controller and service are not same", 
+				customerController.getAll(),
+		customerService.getCustomers());
+	}
+	
+}

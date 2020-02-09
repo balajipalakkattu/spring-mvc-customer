685e10dee1cf5ceada1d549926844bc5d92e62c7
balajipalakkattu <balajipalakkattu@gmail.com>
2020-02-09 01:58:37 -0600

    Unit test:Removed unwanted dependencies

--- src/test/java/com/example/customer/controller/CustomerControllerTest.java
+++ src/test/java/com/example/customer/controller/CustomerControllerTest.java
@@ -5,18 +5,12 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.customer.model.Customer;
